4051a73ef5468254cff919b786b467d934599c5f
Dionne <mylife33@hotmail.com>
2024-09-20 12:17:38 -0500

    remove warning

--- src/components/Views/Resources.js
+++ src/components/Views/Resources.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const clearStyle = {
-  textDecoration: "none",
-  color: "black",
-};
+// const clearStyle = {
+//   textDecoration: "none",
+//   color: "black",
+// };
 
 const Resources = () => {
   return (
